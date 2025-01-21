03cb90c1565b60647c1a20e036fda058b86628be
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-01-21 11:58:27 +0100

    Suppression de la file liste.js qui ne sert à rien

--- src/liste.js
+++ src/liste.js
@@ -1,19 +1,1 @@
-const movieListContainer = document.querySelector('.movie-list');
-const myList = JSON.parse(localStorage.getItem('myList')) || [];
-
-if (myList.length === 0) {
-    movieListContainer.innerHTML = '<p>Aucun film dans votre liste pour le moment.</p>';
-} else {
-    myList.forEach(movie => {
-        const movieItem = `
-            <div class="movie-item">
-                <img src="${movie.poster}" alt="${movie.title}">
-                <h5>${movie.title}</h5>
-            </div>
-        `;
-        movieListContainer.innerHTML += movieItem;
-    });
-}
-
-
 
